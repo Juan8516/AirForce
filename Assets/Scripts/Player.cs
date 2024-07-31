640fa45eea655fbf34596e10af6f1de01d24dcc0
Juan8516 <juan.ind85@gmail.com>
2024-07-31 12:14:51 -0500

    feat: Implementation of lateral and vertical movement for the player
    - Added support for lateral movement (left and right) of the player in the Player script.
    - Incorporated vertical movement (up and down) of the player in the same script.
    - Configured input controls to handle player movement.
    - Adjusted the stage boundaries to prevent the player from leaving the screen.
    - Tested and adjusted player movement behavior to ensure a smooth game experience.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //Variables
+    public int speed = 5;
+
+    [SerializeField]
+    private GameObject _prefabPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Reset position
+        transform.position = new Vector3(0, -4.2f, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovementPlayer();
+        
+        LimitsPlayer();
+    }
+
+    private void MovementPlayer()
+    {
+        //Movement horizontal and vertical of player
+        float inputHorizontal = Input.GetAxis("Horizontal");
+        float inputVertical = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.right * Time.deltaTime * speed * inputHorizontal);
+        transform.Translate(Vector3.up * Time.deltaTime * speed * inputVertical);
+    }
+
+    private void LimitsPlayer()
+    {
+        //Limits player in X
+        if (transform.position.x > 9.2f)
+        {
+            transform.position = new Vector3(-9.2f, transform.position.y, 0);
+        }
+        else if (transform.position.x < -9.2f)
+        {
+            transform.position = new Vector3(9.2f, transform.position.y, 0);
+        }
+
+        //Limits player in Y
+        if (transform.position.y > 4.3f)
+        {
+            transform.position = new Vector3(transform.position.x, -4.3f, 0);
+        }
+        else if (transform.position.y < -4.3f)
+        {
+            transform.position = new Vector3(transform.position.x, 4.3f, 0);
+        }
+    }
+}
